26553b0d8fbf91488ca0e81adb2bd468968dd5b7
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-28 17:04:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "tweets#index"
+  devise_for :users
+  resources :users, only: :create
+  resources :tweets do
+    resources :comments
+  end
 end
