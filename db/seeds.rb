9d502edeeb6b763f11fe059a296c5e2c712b1824
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-28 16:52:25 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+
+  User.create(
+    :nickname => "testuser#{n+1}",
+    :email => "testuser#{n+1}@gmail.com",
+    :password => "testuser#{n+1}"
+  )
+
+  Tweet.create(
+    :text => "testtweet#{n+1}",
+    :iamge => "https://1.bp.blogspot.com/-noejtvMJM8Q/V4SA7f_DLbI/AAAAAAAA8OM/Ajwehtq2jCgO2QvPdxGJz5I290VyUZVLQCLcB/s800/dog_akitainu.png",
+    :user_id => (n+1)
+  )
+
+  Comment.create(
+    :text => "test#{n+1}",
+    :user_id => (n+1),
+    :tweet_id => (n+1)
+  )
+
+end
