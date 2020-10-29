17d5460935f57f8f99d52f972d710fa135652fb9
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-29 13:09:50 +0900

    initialize

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,51 +1,23 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet_id, only: [:show, :edit, :update, :destroy]
-
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
-    @tweet = Tweet.new
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
-    if @tweet.save
-    else
-      render :new
-    end
   end
 
   def show
-    @comment = Comment.new
   end
 
   def edit
   end
 
   def update
-    if @tweet.update(tweet_params)
-    else
-      render :edit
-    end
   end
 
   def destroy
-    if @tweet.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
-  end
-
-  private
-  def tweet_params
-    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
-  end
-
-  def set_tweet_id
-    @tweet = Tweet.find(params[:id])
   end
 end
 
