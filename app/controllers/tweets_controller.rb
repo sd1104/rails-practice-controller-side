a7cf1e5156c48b0bfa7dd819b4dcbcce12333713
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-31 18:25:14 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet_id, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tweets = Tweet.includes(:user).order("create_at DESC")
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -16,6 +18,49 @@ class TweetsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet_id
+    @tweet = Tweet.find(params[:id])
+  end
+
+end
+
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
   end
 
   def edit
@@ -26,86 +71,4 @@ class TweetsController < ApplicationController
 
   def destroy
   end
-
-  private
-  def set_tweet_id
-    params.require(:tweet).permit(:text, :image).merge(:user_id current_user.id)
-  end
-
-
 end
-
-# class TweetsController < ApplicationController
-#   def index
-#   end
-
-#   def new
-#   end
-
-#   def create
-#   end
-
-#   def show
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#   end
-
-#   def destroy
-#   end
-# end
-
-
-# class TweetsController < ApplicationController
-#   before_action :set_tweet_id, only: [:show, :edit, :update, :destroy]
-
-#   def index
-#     @tweets = Tweet.includes(:user).order("created_at DESC")
-#   end
-
-#   def new
-#     @tweet = Tweet.new
-#   end
-
-#   def create
-#     @tweet = Tweet.new(tweet_params)
-#     if @tweet.save
-#     else
-#       render :new
-#     end
-#   end
-
-#   def show
-#     @comment = Comment.new
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#     if @tweet.update(tweet_params)
-#     else
-#       render :edit
-#     end
-#   end
-
-#   def destroy
-#     if @tweet.destroy
-#       redirect_to root_path
-#     else
-#       render :show
-#     end
-#   end
-
-#   private
-#   def tweet_params
-#     params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
-#   end
-
-#   def set_tweet_id
-#     @tweet = Tweet.find(params[:id])
-#   end
-# end
