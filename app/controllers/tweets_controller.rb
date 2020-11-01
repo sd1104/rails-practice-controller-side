a95d9de5d54b18f34b6c900dfe8aa0912e0c692a
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-01 11:51:26 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet_id, only: [:show, :edit, :update, :destroy]
   def index
     @tweets = Tweet.indcluedes(:user).order("created_at DESC")
   end
@@ -29,6 +30,11 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet_id
+    @tweet = Tweet.find(params[:id])
+  end
 end
 
 # class TweetsController < ApplicationController
