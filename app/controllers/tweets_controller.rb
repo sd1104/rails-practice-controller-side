9a81b19ce2241ed093baaad2c12d5792a73ef205
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-05 10:42:04 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet_id, only: [:show, :edit, :destroy]
   def index
-    @tweet = Tweet.indlude(:user).order("created_ta DESC")
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
