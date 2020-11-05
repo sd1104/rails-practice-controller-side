fdceccc959b7b65502ed83284e613f68873ce2fe
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-05 10:41:17 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,6 +31,11 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
